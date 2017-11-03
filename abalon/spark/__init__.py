2e83d153a63389292cdebb455077eb2d14c09dad
Ruslan Dautkhanov <Tagar@users.noreply.github.com>
2017-11-02 22:58:21 -0600

    undo __init__ hack

--- abalon/spark/__init__.py
+++
@@ -1,7 +0,0 @@
-
-# FIXME:
-from .sparkutils import sparkutils_init
-if 'spark' in globals():
-    # assume global variable 'spark' references SparkSession
-    sparkutils_init(globals()['spark'])
-
