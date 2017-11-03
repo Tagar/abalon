530d191bcaeb95d253dd17702d7067b26d30196e
Ruslan Dautkhanov <Tagar@users.noreply.github.com>
2017-11-02 22:42:43 -0600

    don't require sparkutils_init() to be called explicitly

--- abalon/spark/__init__.py
+++ abalon/spark/__init__.py
@@ -1,6 +1,7 @@
 
-__all__ = ["sparkutils_init"
-         , "file_to_df", "sql_to_df"
-         , "HDFScopyMerge", "dataframeToHDFSfile"
-         ]
+# FIXME:
+from .sparkutils import sparkutils_init
+if 'spark' in globals():
+    # assume global variable 'spark' references SparkSession
+    sparkutils_init(globals()['spark'])
 
