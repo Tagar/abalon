5db440b61830d4d562d2fcc7f5d7998d40ef156e
Ruslan Dautkhanov <Tagar@users.noreply.github.com>
2017-11-06 16:43:13 -0700

    fix filenames sorting
    1. fix filenames sorting
    2. move version to a separate file

--- abalon/spark/sparkutils.py
+++ abalon/spark/sparkutils.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 ###########################################################################################################
@@ -118,7 +131,7 @@ def HDFScopyMerge (src_dir, dst_file, overwrite=False, deleteSource=False):
     files = []
     for f in fs.listStatus(hadoop.fs.Path(src_dir)):
         if f.isFile():
-            files.append(f.getPath())
+            files.append(str(f.getPath()))
     if not files:
         raise ValueError("Source directory {} is empty".format(src_dir))
     # determine order of files in which they will be written:
